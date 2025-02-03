180b06cb9e79d4522cf4263aab9d99b70f09e8d2
Chris <chris79gp@outlook.com>
2025-02-03 15:41:10 +0000

    Chore: remove placeholder content from all pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,39 +1,8 @@
-import Hero from "/src/components/Hero/Hero";
-import Headline from "/src/components/Headline/Headline";
-import List from "/src/components/List/List";
-
 const About = function () {
 	return (
 		<>
-			<Hero heading="Business Coaching" description="An independent view to help you grow" />
 			<section className="fb-col-wrapper">
-				<Headline heading="Unleash the potential of your business">
-					<p>
-						Running a business takes knowledge, energy, communication and imagination. It also takes
-						up all of your time.
-					</p>
-					<p>
-						Large organisations have support structures where you can get help, but when you run a
-						small business it can feel like you have to solve every problem yourself, and it can be
-						a lonely place.
-					</p>
-					<p>
-						This is where business coaching comes in. Business coaching is not about telling you how
-						to run your business, it is about drawing from you a way forward and a plan to succeed.
-					</p>
-					<List
-						listHeading="Business coaching can help you with"
-						listItems={[
-							"Overcoming barriers to growth",
-							"Strategic planning",
-							"Change management",
-							"Project management",
-							"Cashflow management",
-							"Finding great people to employ",
-							"Selling and marketing",
-						]}
-					/>
-				</Headline>
+				<h1>About</h1>
 			</section>
 		</>
 	);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,9 @@
-import Headline from "/src/components/Headline/Headline";
 import Hero from "/src/components/Hero/Hero";
-import List from "/src/components/List/List";
 
 const Home = function () {
 	return (
 		<>
 			<Hero />
-			<section className="fb-col-wrapper">
-				<Headline heading="About">
-					<p>How many times have you thought &quot;I need help!&quot;</p>
-					<p>
-						We are here to help. We help people to understand themselves, their work colleagues and
-						their business. We do this by listening to you, and drawing from you your ideas to help
-						yourself.
-					</p>
-					<p>
-						Real progress comes from within. Getting through the stress, the fog, or the noise is
-						always the barrier to finding your own solution, and that&apos;s what we do, help you
-						get through all the noise, think clearly and work out how to execute.
-					</p>
-					<List
-						listHeading="We do this by"
-						listItems={[
-							"Giving you time to talk about what is important to you.",
-							"Listen to what you say, and how you say it. We consider the tone of your voice and the body language to spot congruence and incongruence.",
-							"Help you articulate the issue to be dealt with.",
-							"Draw out your ideas.",
-							"Build out a road map so that the actions and objectives are clear to you.",
-						]}
-					/>
-				</Headline>
-			</section>
 		</>
 	);
 };
