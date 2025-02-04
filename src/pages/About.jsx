63a54d97ecebb713b793389a6f1d72c10917d3da
Chris <chris79gp@outlook.com>
2025-02-04 15:58:45 +0000

    Feat: add global margin and padding to `section` and `h1` elements

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,11 @@
+import Biography from "../components/Biography/Biography";
+
 const About = function () {
 	return (
 		<>
 			<section className="fb-col-wrapper">
 				<h1>About</h1>
+				<Biography />
 			</section>
 		</>
 	);
