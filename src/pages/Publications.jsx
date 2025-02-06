6820a9264855a1df064d9c1abf097c9ff4f114ae
Chris <chris79gp@outlook.com>
2025-02-06 11:22:00 +0000

    Feat: add content for "Publications" page

--- src/pages/Publications.jsx
+++ src/pages/Publications.jsx
@@ -1,7 +1,21 @@
+import Publication from "../components/Publication/Publication";
+import pubsData from "/src/data/publications.json";
+import "./Publications.scss";
+
 const Publications = function () {
+	const pubsElements = pubsData.map(pub => (
+		<Publication
+			key={pub.id}
+			title={pub.title}
+			instruments={pub.instruments}
+			description={pub.description}
+		/>
+	));
+
 	return (
-		<section className="fb-col-wrapper">
-			<h1>Publications</h1>
+		<section className="publications fb-col-wrapper">
+			<h1 className="publications__heading">Publications</h1>
+			<div className="publications__publications">{pubsElements}</div>
 		</section>
 	);
 };
