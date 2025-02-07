f765c778bc78e66671c9c9872f23cab1ddbec387
Chris <chris79gp@outlook.com>
2025-02-07 17:46:01 +0000

    Feat: update `List` component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,24 +1,29 @@
 import PropTypes from "prop-types";
 import "./List.scss";
 
-const List = function ({ listHeading, listItems }) {
+const List = function ({ items, description, isOrdered = false }) {
+	const itemElements = items.map((item, i) => (
+		<li key={i} className="list__item">
+			{item}
+		</li>
+	));
+
 	return (
 		<div className="list">
-			<h3 className="list__heading">{listHeading}</h3>
-			<ul className="list__items-container">
-				{listItems.map((listItem, i) => (
-					<li key={i} className="list__item">
-						{listItem}
-					</li>
-				))}
-			</ul>
+			<p className="list__description">{description}</p>
+			{isOrdered ? (
+				<ol className="list__items-container ordered">{itemElements}</ol>
+			) : (
+				<ul className="list__items-container unordered">{itemElements}</ul>
+			)}
 		</div>
 	);
 };
 
 List.propTypes = {
-	listHeading: PropTypes.string,
-	listItems: PropTypes.arrayOf(PropTypes.string),
+	items: PropTypes.arrayOf(PropTypes.string).isRequired,
+	description: PropTypes.string,
+	isOrdered: PropTypes.bool,
 };
 
 export default List;
