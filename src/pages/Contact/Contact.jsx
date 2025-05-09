b088cde17d67670b7bd123d77e7738e4271423f5
Chris <chris79gp@outlook.com>
2025-05-09 18:07:49 +0100

    Feat: add styling and image for `Contact` page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,24 @@
 import ContactForm from "./ContactForm/ContactForm.jsx";
+import "./Contact.scss";
+
+import ta from "/src/assets/images/terence-allbright-03.jpg";
 
 const Contact = function () {
 	return (
 		<>
-			<section className="fb-col-wrapper">
+			<section className="contact fb-col-wrapper">
 				<h1>Contact</h1>
-				<ContactForm />
+				<div className="contact__wrapper">
+					<div className="contact__form-wrapper">
+						<h2>Leave a message for Terry</h2>
+						<ContactForm />
+					</div>
+					<img
+						src={ta}
+						alt="Terry peering through a window at a poster for his concert."
+						className="contact__image"
+					/>
+				</div>
 			</section>
 		</>
 	);
